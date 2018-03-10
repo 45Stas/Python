9895cccc5c532f171c76ff7d4a2e84efbc8ad32b
Stas <37015391+45Stas@users.noreply.github.com>
2018-03-10 16:00:17 +0600

    Тут ошибки

--- Program3.py
+++ Program3.py
@@ -1,6 +1,20 @@
-number = int(input ("Начальное число"))
+number = input ("Начальное число")
 numbe = int(input("Конечное число"))
 numb = int(input("Диапазон числа"))
-while number <= numbe:
-    print(number)
-    number += numb
+if number != "":
+    while number <= numbe:
+        print(number)
+        number += numb
+else:
+    print ("Error")
+if number > numbe:
+    print ("Поменьше поставь начальное!")
+elif numbe <= 0:
+    print ("Ноль убери")
+elif numb <= 0:
+    print ("Ноль убери") 
+else:
+    
+
+
+   
