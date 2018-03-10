46b5728cd9f6701621ef2df3938148c2ac391599
Stas <37015391+45Stas@users.noreply.github.com>
2018-03-10 17:40:20 +0600

    Считать денюжку

---
+++ Program6.py
@@ -0,0 +1,24 @@
+
+def main():
+    sum=0
+    count =int(input("Скоко бумажек у тебя в кошельке"))
+    for i in range(1, count + 1, 1):
+        money = int(input("Какая цифарка на бумажке"))
+        if proverka(money) == True:
+            sum+=money
+        result(sum)
+
+
+        
+def proverka(money):
+    if money == 200 or money == 500 or money == 1000 or money == 2000 or money == 5000 or money == 10000 or money == 20000:
+        return True
+    else:
+        print("Ща полисию вызову")
+        return False
+def result(suma):
+    print(suma)
+    
+    
+main()
+
