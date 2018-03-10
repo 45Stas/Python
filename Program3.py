cfce722d2c04a3a1c9ecf1077feea556578d7da9
Stas <37015391+45Stas@users.noreply.github.com>
2018-03-10 16:25:35 +0600

    Цикл

--- Program3.py
+++ Program3.py
@@ -1,19 +1,17 @@
-number = input ("Начальное число")
+number = int(input ("Начальное число"))
 numbe = int(input("Конечное число"))
 numb = int(input("Диапазон числа"))
-if number != "":
-    while number <= numbe:
-        print(number)
-        number += numb
-else:
-    print ("Error")
 if number > numbe:
     print ("Поменьше поставь начальное!")
 elif numbe <= 0:
     print ("Ноль убери")
 elif numb <= 0:
     print ("Ноль убери") 
-else:
-    
+else: 
+    while number <= numbe:
+        print(number)
+        number += numb
+print ("Goodbye")
+        
 
 
