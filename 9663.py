06c333f9ef222a062abf395625b39791dafc79d6
Stas <37015391+45Stas@users.noreply.github.com>
2018-04-14 15:01:50 +0600

    гх

---
+++ 9663.py
@@ -0,0 +1,18 @@
+a = int(input("Введите число номер 1 "))
+b = int(input("Введите число номер 2 "))
+z = input("Введите знак")
+if z == "+":
+    c = a + b
+    print(c)
+elif z == "-":
+    c = a - b
+    print(c)
+elif z == "/":
+    c = a / b
+    print(c)
+elif z == "*":
+    c = a * b
+    print(c)
+       
+    
+
